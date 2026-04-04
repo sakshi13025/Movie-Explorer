39607856bc309a486351681270befc610bcfe738
sakshi13025 <sakshi13025@gmail.com>
2026-04-04 09:31:15 +0530

    UI Part Done

--- src/Pages/MovieDetails.jsx
+++ src/Pages/MovieDetails.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import { getMovieDetails } from "../services/apiService"
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { addToFav, removeFromFav, isFav } from "../components/fav"
-import { toast } from 'react-toastify'
+import React from "react";
+import { getMovieDetails } from "../services/apiService";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { addToFav, removeFromFav, isFav } from "../components/fav";
+import { toast } from "react-toastify";
 
 function MovieDetails() {
-  
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+  const [fav, setFav] = useState(false);
+
   return (
     <div>
       <h1>Movie Details</h1>
     </div>
-  )
+  );
 }
 
-export default MovieDetails
+export default MovieDetails;
